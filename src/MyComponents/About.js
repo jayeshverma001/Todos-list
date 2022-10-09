a8a02fd818aa8701b1b01df78c42fa077946285d
Jayesh Kumar Verma <111409049+jayeshverma001@users.noreply.github.com>
2022-10-09 19:39:31 +0100

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      About Todos list :
+      <p>
+        Having a list provides order and, if done regularly, it develops your
+        discipline which is a good habit for students to have. To really develop
+        the virtue of order, every student should have a structured process to
+        fill each item in the To-Do list.
+        <h5> How To Write A To Do List</h5>
+        You'll Actually Stick To Think about the bigger picture to work out your
+        priorities. Add some structure to your to-do list. Keep your to-do list
+        to a manageable size.Be specific with your tasks. Calendarize your to-do
+        list.
+      </p>
+    </div>
+  );
+};
